4cd479b1a0e0d5507809eb27a6697d4693677b6a
Kakashi <nguyenvanca2110@gmail.com>
2023-02-07 00:56:26 +0900

    Add check host IP

--- src/main/java/com/example/gradledemo/controller/WelcomeResource.java
+++ src/main/java/com/example/gradledemo/controller/WelcomeResource.java
@@ -4,6 +4,8 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 @RestController
 @RequestMapping("/")
@@ -12,5 +14,12 @@ public class WelcomeResource {
     @GetMapping(value="greetings", produces = MediaType.APPLICATION_JSON_VALUE )
     public String greetings(){
         return "Welcome To Gradle 2!";
+    }
+
+    @GetMapping("/local")
+    public String getLocalIp() throws UnknownHostException {
+        InetAddress inetAddress = InetAddress.getLocalHost();
+        String localIp = inetAddress.getHostAddress();
+        return "Local IP address : " + localIp;
     }
 }
